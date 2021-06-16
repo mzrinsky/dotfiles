4a75c85cc9265abb600d7ef8bf596f27b3274e09
Matt Zrinsky <matt+git@zrinsky.us>
2021-06-16 09:19:23 -0500

    Add reboot required section
    Just something I use on ubuntu server machines.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -4,6 +4,14 @@
 function fish_prompt
   set --local ret_status $status
   set --local now_time (date '+%m/%d/%Y %r')
+# you can uncomment this section, if you are on ubuntu, and want to know when you should reboot ;)
+# this is a varient of my config I use on ubuntu server.
+#  if test -f "/var/run/reboot-required"
+#    set_color ff005f
+#    echo -en "\n"
+#    cat -s /var/run/reboot-required | sed -E 's/\n//g' | sed -E 's/^/    /g' 
+#    set_color normal
+#  end 
   echo -e "\n  "(set_color "$fish_color_dark_gray")"new prompt "(set_color "$fish_color_light_gray")"@"(set_color "$fish_color_gray")" $now_time\n"
   set -l realhome ~
   set -l cdir (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
